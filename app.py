7c85c890277cde2e5bcc77b702d4d3e9e949483a
Renan <renanmunizandrade@hotmail.com>
2024-11-02 17:06:12 -0300

    Alpha project done

--- app.py
+++ app.py
@@ -7,109 +7,109 @@ db_cosing = pd.read_csv('.\data\db_cosing.csv', delimiter=';', encoding='utf-8-s
 
 db_products['Código de Barras'] = [db_products['Código de Barras'][i].replace('"', '') for i,row in db_products.iterrows()]
 
-def get_product_function(ean):
+def find_closest_inci_name(component, inci_names):
+    """ Encontra o nome INCI mais próximo baseado na regex. """
+    # Escapa caracteres especiais em componentes para uso em regex
+    pattern = re.escape(component.strip().lower())
+    best_match = None
+    best_score = 0
 
-    functions = {}
+    for inci_name in inci_names:
+        # Verifica quantas palavras em `component` aparecem em `inci_name`
+        matches = re.findall(pattern, inci_name.lower())
+        score = len(matches)
+        if score > best_score:
+            best_score = score
+            best_match = inci_name
+
+    return best_match
+
+
+def get_product_function(ean):
+    product_infos = {
+        'functionsPerComposition': {},
+        'functionOccurrences': {},   # Guarda a primeira ocorrência da composição e a ordem geral de cada função
+        'functionPerOcurrenceNumber': {},  # Contagem de ocorrências para cada função
+    }
+    
+
+    composition_count = 0
+    all_functions = []
 
     for i, row in db_products.iterrows():
         if row['Código de Barras'] == ean:
-            compositions = row['Composição']
-            compositions = compositions.split(sep=',')
-            # print(compositions)
 
+            product_infos['ref'] = row['REF']
+            product_infos['brand'] = row['Marca']
+            product_infos['name'] = row['Nome do Produto']
+            product_infos['ean'] = ean
+            product_infos['composition'] = [composition.strip() for composition in row['Composição'].split(sep=',')]
+
+            compositions = [comp.strip() for comp in row['Composição'].split(',')]
             for composition in compositions:
-                composition = composition.strip()
+                composition_count += 1
+                inci_names = [cosing_row['INCI name'] for cosing_index, cosing_row in db_cosing.iterrows()]
+                closest_inci = find_closest_inci_name(composition, inci_names)
+                
+                # Após encontrar o INCI mais próximo, obtenha as funções relacionadas
+                if closest_inci:
+                    matched_row = db_cosing[db_cosing['INCI name'].str.contains(re.escape(closest_inci), case=False, regex=True)]
+                    if not matched_row.empty:
+                        function_list = [func.strip() for func in matched_row.iloc[0]['Function PT'].split(',')]
+                        product_infos['functionsPerComposition'][composition] = [func.lower() for func in function_list]
+                        for function in function_list:
+                            function_lower = function.lower()
+                            if function_lower not in product_infos['functionOccurrences']:
+                                all_functions.append(function_lower)
+                                product_infos['functionOccurrences'][function_lower] = [composition_count, len(all_functions)]
+                            
+                            # Adiciona ou incrementa a contagem de ocorrências de cada função
+                            if function_lower in product_infos['functionPerOcurrenceNumber']:
+                                product_infos['functionPerOcurrenceNumber'][function_lower] += 1
+                            else:
+                                product_infos['functionPerOcurrenceNumber'][function_lower] = 1
 
-                for j, cosing_row in db_cosing.iterrows():
-                    # print(cosing_row['INCI name'].strip())
-                    if composition.upper() == cosing_row['INCI name'].strip():
-                        print('achei!')
-                        functions[composition] = cosing_row['Function PT'].split(sep=',')
-                    
-                if composition not in functions.keys():
-                    functions[composition]: 'Não encontrado'
-
-    all_composition = []
-    for composition in functions.keys():
-        all_composition += functions[composition]
+    return product_infos
 
 
+def calculate_function_score(product_infos):
+    # Pesos: Suponha que a frequência é duas vezes mais importante que a posição
+    peso_frequencia = 2.0
+    peso_posicao = 1.0
 
-    # print(all_composition)
+    max_occurrence = max(product_infos['functionPerOcurrenceNumber'].values())  # Máximo de ocorrências para normalização
+    min_position = min(val[0] for val in product_infos['functionOccurrences'].values())  # Mínima posição para normalização
 
-    print(functions)
-                    
-            
-        
-get_product_function('7898305625775')
-     
-'''
-def get_db_connection():
-    try:
-        server = 'DANIELKUNIHIRO\MSSQLSERVER01'  # Ou 'localhost\\SQLEXPRESS'
-        database = 'db_product-inci'
-        conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes', timeout=5)
-        return conn
-    except Exception as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
-        return None  # Retorna None para indicar falha
+    function_scores = {}
 
+    for function, details in product_infos['functionOccurrences'].items():
+        # Normalizando valores
+        normalized_occurrence = product_infos['functionPerOcurrenceNumber'][function] / max_occurrence
+        normalized_position = (min_position / details[0])  # Inverte para que menor posição tenha maior score
+
+        # Cálculo do score
+        score = (peso_frequencia * normalized_occurrence) + (peso_posicao * normalized_position)
+        function_scores[function] = float(f"{score:.2f}")
+
+    return function_scores
 
 # Criar a aplicação em Flask
 app = Flask(__name__)
-
-def get_products():
-    # Função para retornar uma lista de produtos ou as informações de um produto específicado
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    # Recuperando o código de barras da URL se fornecido
-    barcode = request.args.get('ean', default=None)
-
-    # Modificar a consulta SQL com base na presença do códigto de barras
-    if barcode:
-        cursor.execute("EXEC function_info_by_productEan @ean = ?", (barcode,))
-        # cursor.execute('SELECT * FROM db_products WHERE código_de_barras = ?', (barcode,))
-    else: 
-        cursor.execute('SELECT * FROM db_products')
-    
-    products = cursor.fetchall()
-    cursor.close()
-    conn.close()
-
-    # Conveter os resultados em uma lista de dicionários para jsonify
-    # products_list = [{'ref': product[0],'brand': product[1] ,'name': product[2], 'cateogory': product[3], 'price': product[4], 'description': product[5],'ean': barcode ,'use_mode': product[7], 'composition': product[8]} for product in products]
-    products_list = [{'function': product[0], 'qtd': product[1]} for product in products]
-    return jsonify(products_list)
-
-
-
     
 @app.route('/product-info', methods=['GET'])
 def get_product_info():
     barcode = request.args.get('ean', default=None)
     if not barcode:
         return jsonify({'error': 'EAN code is required'}), 400
+    product_info = get_product_function(barcode)
+    product_info['functionScores'] = {}
+    product_info['functionScores'] = calculate_function_score(product_info)
     
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    try:
-        sql = 'SET NOCOUNT ON; EXEC function_info_by_productEan @ean=?'
-        values = (barcode)
-        cursor.execute(sql, (values))
-        # cursor.execute("EXEC function_info_by_productEan(?)", (barcode,))
-        results = cursor.fetchall()
-        # Assumindo que a função retorna colunas específicas, ajuste conforme necessário
-        product_info = [{'function': row[0], 'qtd': row[1]} for row in results]
-        return jsonify(product_info)
-    except Exception as e:
-        return jsonify({'error': 'Failed to fetch data', 'message': str(e)}), 500
-    finally:
-        cursor.close()
-        conn.close()
+    return jsonify(product_info)
+
+@app.route('/product-has-function', methods=['GET'])
+def get_product_functions():
+    pass
 
 if __name__ == '__main__':
     app.run()
-
-
-'''
